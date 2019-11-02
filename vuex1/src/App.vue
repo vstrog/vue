b0fae9bf75a352269f46fddf1897d2c40ab162ba
vstrog <39245801+vstrog@users.noreply.github.com>
2019-11-02 09:58:08 +0200

    first vuex project

---
+++ vuex1/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <PostForm />
+    <h1>Number of Posts: {{ postsCount }}</h1>
+    <div class="post" v-for="post in validPosts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p> {{ post.body }} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+import PostForm from "./components/PostForm"
+export default {
+  name: "app",
+//   computed: {
+//     allPosts() {
+//       return this.$store.getters.allPosts;
+//     }
+// },
+  computed: mapGetters(["validPosts", "postsCount"]),
+  methods: mapActions(["fetchPosts"]),
+  components: { PostForm },
+  async mounted() {
+    //this.$store.dispatch("fetchPosts");
+    this.fetchPosts();
+  }
+}
+</script>
+
+<style>
+h1 {
+   color: #b48608; font-family: 'Droid serif', serif; font-size: 36px; font-weight: 400; font-style: italic; line-height: 44px; margin: 0 0 12px; text-align: center; 
+}
+#app {
+  
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 60px auto;
+  width: 400px;
+}
+
+.post {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+
+</style>
---
+++ vuex1/src/components/PostForm.vue
@@ -0,0 +1,67 @@
+<template>
+    <form @submit.prevent="submit">
+        <input type="text" placeholder="title" v-model="title">
+        <input type="text" placeholder="body" v-model="body">
+        <button class="btn" type="submit">Create Post</button>
+    </form>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    ...mapMutations(["createPost"]),
+    submit() {
+      this.createPost({
+        title: this.title,
+        body: this.body,
+        id: Date.now()
+      });
+      this.title = this.body = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+input[type=text] {
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+  width: 100%;
+}
+input[type=text]:focus {
+  width: 100%;
+}
+
+.btn {
+	background:linear-gradient(to bottom, #44c767 5%, #5cbf2a 100%);
+	background-color:#44c767;
+	border:3px solid #18ab29;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:18px;
+	padding:12px 29px;
+	text-decoration:none;
+	text-shadow:3px 6px 9px #2f6627;
+  margin-bottom: 1.5rem;
+}
+.btn:hover {
+	background:linear-gradient(to bottom, #5cbf2a 5%, #44c767 100%);
+	background-color:#5cbf2a;
+}
+.btn:active {
+	position:relative;
+	top:1px;
+}
+
+        
+</style>
